61e2f2fae0a39594e86f495297577e61d0574690
John Moore <johnmoore314@gmail.com>
2016-08-10 23:27:52 -0500

    Add conditionals to change client information to hashes.rb

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -14,19 +14,18 @@
 #store user information in variables
 
 #display user information as a hash
+###################################################
 
-i = 0 #used in later loop
-client = {} #necessary for filling in content
+i = 0
+client = {}
+
+#an affirmative statement to include a variety of "yes" answers used later on
+affirmative = ["y", "yes", "yeah", "sure", "definitely", "hell yes", "absolutely"]
 
 #Define a method for entering information into hash
 def hashing(client, key, value)
 	client[key.to_sym] = value
 end
-
-#testing initial method
-#hashing(client, "me", 31)
-#hashing(client, "you", 31)
-#puts client
 
 #Ask and store user information
 puts "What is the client's full name?"
@@ -38,7 +37,6 @@ age = gets.chomp.to_i
 puts "How many children does the client have?"
 children = gets.chomp.to_i
 
-#putting nil for children if none
 if children == 0 
 	children = nil
 end
@@ -46,59 +44,96 @@ end
 puts "Does the client like paisley wallpaper?"
 paisley = gets.chomp.to_s.downcase
 
-#making paisley boolean
-if paisley == "y" || "yes" || "yeah" || "sure" || "definitely" || "hell yes"
-	paisley == true
+if affirmative.include?(paisley) 
+	paisley = true
 else
-	paisley == false
+	paisley = false
 end
 
 puts "Does the client like marble counter-tops?"
-marble = gets.chomp
+marble = gets.chomp.to_s.downcase
 
-if marble == "y" || "yes" || "yeah" || "sure" || "definitely" || "hell yes"
-	marble == true
+if affirmative.include?(marble)
+	marble = true
 else
-	marble == false
+	marble = false
 end
 
-puts "What is the clients favourite type of wood?"
+puts "What is the client's favourite type of wood?"
 wood = gets.chomp
 
 puts "Does the client prefer an open concept home?"
-open_concept = gets.chomp
+open_concept = gets.chomp.to_s.downcase
 
-if paisley == "y" || "yes" || "yeah" || "sure" || "definitely" || "hell yes"
-	paisley == true
+#reversing affirmative answers, just for testing theory of code
+if !affirmative.include?(open_concept)
+	open_concept = false
 else
-	paisley == false
+	open_concept = true
 end
 
 puts "What is the client's preferred interior design theme?"
 design_theme = gets.chomp
 
 puts "Does the client have any other preferences for design?"
-other = gets.chomp
+other = gets.chomp.to_s.downcase
 
-if paisley == "y" || "yes" || "yeah" || "sure" || "definitely" || "hell yes"
-	paisley == true
+other_design_preferences = []
+
+if affirmative.include?(other)
+	other = true
 else
-	paisley == false
+	other_design_preferences = nil
+end
+
+if other == true 
+    puts "What other preference of design does the client have?\n Please enter these preferences one at a time.\n When completed, simply enter \"done\""
+while other == true
+	other_design_preferences << gets.chomp
+	break if other_design_preferences.last == "done"
+    end
 end
 
 #storing user information in strings to later use in a loop
-keys = ["Name", "Age", "Children", "Likes_paisley", "Likes_Marble", "Preference_of_wood", "Open_Concept", "Design_Theme", "Other"]
-values = [name, age, children, paisley, marble, wood, open_concept, design_theme, other]
+keys = ["Name", "Age", "Children", "Likes_paisley", "Likes_marble", "Preference_of_wood", "Open_concept", "Design_theme", "Other"]
+values = [name, age, children, paisley, marble, wood, open_concept, design_theme, other_design_preferences]
 
 #starting loop for inputting information into the client hash
-if keys.length == values.length 			#works for testing code
-	while i < keys.length
-		key = keys[i]
-		value = values[i]
-		hashing(client, key, value)
-		i += 1
+while i < keys.length
+	key = keys[i]
+	value = values[i]
+	hashing(client, key, value)
+	i += 1
+end
+
+puts client
+
+#Changing information conditionals
+puts "Would you like to change any of the client information?"
+change = gets.chomp.to_s.downcase
+
+if affirmative.include?(change)
+	puts "Which information would you like to change?"
+	info = gets.chomp.to_s.capitalize
+	if keys.include?(info)
+		puts "Which value would you like to assign this key?"
+		value = gets.chomp
+		puts "Lets process that change for you."
+		hashing(client, info, value)
+		puts client
+	else 
+		puts "This looks like a new element in the hash,\n Shall we process some new information for you?"
+		new_change = gets.chomp
+		if affirmative.include?(new_change)
+			puts "Ok, which value would you like to assign this key?"
+			value = gets.chomp
+			puts "Lets process that addition for you."
+			hashing(client, info, value)
+			puts client
+		else 
+			puts "Alright. Thank you for your time."
+		end
 	end
-	puts client
-else 
-	puts "There was an error inputting client information, please try again"
+else
+	puts "Alright. Thank you for your time."
 end
