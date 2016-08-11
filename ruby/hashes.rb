ee1b4634f1b99b6b630f1f3f1cd275c0bef0fb7c
John Moore <johnmoore314@gmail.com>
2016-08-10 19:54:27 -0500

    Add loop to add information to hash in hashes.rb

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -54,3 +54,19 @@ design_theme = gets.chomp
 puts "Does the client have any other preferences for design?"
 other = gets.chomp
 
+#storing user information in strings to later use in a loop
+keys = ["Name", "Age", "Children", "Likes_paisley", "Likes_Marble", "Preference_of_wood", "Open_Concept", "Design_Theme", "Other"]
+values = [name, age, children, paisley, marble, wood, open_concept, design_theme, other]
+
+#starting loop for inputting information into the client hash
+if keys.length == values.length
+	for i in keys.length
+		key = keys[i]
+		value = values[i]
+		hashing(client, key, value)
+		i += 1
+	end
+	puts client
+else 
+	puts "There was an error inputting client information, please try again"
+end
