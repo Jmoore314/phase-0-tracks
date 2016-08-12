55ebdb9c20d825d98c9feaec7338d76336f7ee7f
John Moore <johnmoore314@gmail.com>
2016-08-12 17:36:23 -0500

    Add nested data structure to nested_data_structures.rb

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,100 @@
+voip_services = {
+	teamspeak_3: {
+		server_names: {
+			Phoenix_Rising: {
+				channel_names: [
+					'Apathy Central',
+					'Final Fantasy',
+					'Overwatch'
+				],
+				people_in_server: 5,
+				max_population: 25
+			}
+		}
+	},
+
+	raidcall: {
+		server_name: {
+			Trinity: {
+				channel_names: [
+					'Star Trek Online',
+					'Matrix Online'
+				],
+				people_in_server: 25,
+				max_population: 100
+			},
+			
+			Neo: {
+				channel_names: [
+					'Star Wars Galaxies',
+					'Matrix Online'
+				],
+				people_in_server: 2,
+				max_population: 100
+			}
+		}
+	},
+
+	skype: {
+		people_on_call: [
+			'Dave',
+			'Jenny',
+			'Bart',
+			'Christy',
+			'Tyra',
+			'Hank',
+			'Tasha',
+			'Sam',
+			'Jerome',
+			'Lee'
+		]
+	},
+	
+	ventrillo: {
+		server_name: {
+			Method_Gaming: {
+				channel_name: [
+					'World of Warcraft'
+				],
+				people_in_server: 78,
+				max_population: 250
+			}
+		}
+	},
+
+	mumble: {
+		server_name: {
+			Goon_Squad: {
+				channel_name: [
+					'World of Warcraft',
+					'Descent',
+					'Camping the Stairs',
+					'Destiny'
+				]
+			}
+		}
+	},
+
+	discord: {
+		server_name: { 
+			Goonz: {
+				channel_name: [
+					'Blade and Soul',
+					'Overwatch',
+					'Diablo 3',
+					'Black Desert',
+					'Age of Conan'
+				]
+			},
+
+			Method_Gaming: {
+				channel_names: [
+					'World of Warcraft',
+					'Counter-Strike',
+					'Overwatch',
+					'Heros of the Storm'
+				]
+			}
+		}
+	}
+}
