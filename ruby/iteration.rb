5ead1ba7a36892dee6aac7e198003aba6c3ae8d6
John Moore <johnmoore314@gmail.com>
2016-08-11 20:40:40 -0500

    Add each and map commands to arrays and hashes for iteration.rb

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -6,3 +6,37 @@ def print_stuff
 end
 
 print_stuff { |name1, name2| puts "#{name1} #{name2}"}
+
+#################
+
+array_names = ["Tiff", "John", "Dave", "Kristy"]
+
+hash_ages = { "Tiff" => 20, "John" => 20, "Dave" => 90, "Kristy" => 45 }
+
+#################
+#Before
+p array_names
+
+p hash_ages
+
+#################
+
+array_names.each do |name|
+    puts "Hi #{name}"
+end
+
+array_names.map! do |name|
+    name*2
+end
+
+#################
+
+hash_ages.each do |name, age|
+    puts "#{name} is #{age} years old!"
+end
+
+#################
+#After
+p array_names
+
+p hash_ages
