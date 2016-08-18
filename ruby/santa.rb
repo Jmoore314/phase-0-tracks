33b361973a3d6e10d84429da6a9551797ec98cb5
John Moore <johnmoore314@gmail.com>
2016-08-18 15:51:59 -0500

    Add looping driver code for all santas in santa.rb

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -33,7 +33,7 @@ class Santa
 	def initialize(gender, ethnicity)
 		@gender = gender
 		@ethnicity = ethnicity
-		puts "Initializing Santa instance ..."
+		puts "Initializing Santa instance with #{@gender} gender and #{@ethnicity} ethnicity"
 	end
 
 end
@@ -58,13 +58,26 @@ end
 		#]
 	#age, gets default of 0
 
-santas = []
-santas << Santa.new("agender", "black")
-santas << Santa.new("female", "Latino")
-santas << Santa.new("bigender", "white")
-santas << Santa.new("male", "Japanese")
-santas << Santa.new("female", "prefer not to say")
-santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
-santas << Santa.new("N/A", "N/A")
+#santas = []
+#santas << Santa.new("agender", "black")
+#santas << Santa.new("female", "Latino")
+#santas << Santa.new("bigender", "white")
+#santas << Santa.new("male", "Japanese")
+#santas << Santa.new("female", "prefer not to say")
+#santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+#santas << Santa.new("N/A", "N/A")
 
+santas = []
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+example_genders.length.times do |i|
+  santas << Santa.new(example_genders[i], example_ethnicities[i])
+end
+
+array_of_cookies = ["Chocolate Chip", "Oatmeal Raisin", "Salt", "Shortbread", "Peanut Butter", "Heath Bar", "Sugar"]
+
+santas.each do |santa|
+	santa.speak
+	santa.eat_milk_and_cookies(array_of_cookies[rand(0..6)])
+end
 
