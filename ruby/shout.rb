80ba65c2ea098e9677fdcf3117d2b091b2ad31f0
John Moore <johnmoore314@gmail.com>
2016-08-19 16:32:12 -0500

    Add module and bird, plane classes to flight.rb and mix module with adult, child classes to shout.rb

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,18 +1,31 @@
 module Shout
 
-	def self.yell_angrily(words)
-		words + "!!!" + " :("
+	def yell_angrily(words)
+		puts words + "!!!" + " :("
 	end
 
-	def self.yell_happily(words)
-		words + "!!!" + " ^_^"
+	def yell_happily(words)
+		puts words + "!!!" + " ^_^"
 	end
 
 end
 
+class Child
+	include Shout
+end
+
+class Adult
+	include Shout
+end
+
+
 #DRIVER CODE
 
-require_relative 'shout'
-Shout.yell_angrily("Oh")
-Shout.yell_happily("Oh")
+#require_relative 'shout'
+#Shout.yell_angrily("Oh")
+#Shout.yell_happily("Oh")
+child = Child.new
+child.yell_happily("Thank you for the new toy Mama")
 
+adult = Adult.new
+adult.yell_angrily("Damn, now we'll never get any sleep")
