0a3adcc28da5acdbfeab99160339d3bfc60be207
John M. Moore <johnmoore314@gmail.com>
2016-08-25 21:11:34 -0500

    Add find longest string in array function to algos.js

--- js/algos.js
+++ js/algos.js
@@ -7,3 +7,18 @@ if the string is larger than the first in the new array
 then delete the first in the new array and add the string that is longer
 continue looping through original array until the end and return the new arrays 0 element*/
 
+funciton algos(array) {
+	var new_array = []
+	for (i = 0; i < array.length; i++ ) {
+		if (i == 0) {
+			new_array.push(array[0])
+		} else {
+			if (array[i].length > new_array[0].length) {
+				new_array.splice(0, 1)
+				new_array.push(array[i])
+			}
+		}
+	}
+	console.log("The longest item in the array is:")
+	console.log(new_array[0])
+}
