08105ec2f0169c7e0047572bc055d0e626edec37
John M. Moore <johnmoore314@gmail.com>
2016-08-25 21:00:07 -0500

    Add pseudocode to algos.js

---
+++ js/algos.js
@@ -0,0 +1,9 @@
+/* create a function that takes an array as an argument
+loop through array from 0 to array length
+create a new array that is empty
+if string is first string in array save to the new array
+if second + string in the array then compare string length to the new array first string length
+if the string is larger than the first in the new array
+then delete the first in the new array and add the string that is longer
+continue looping through original array until the end and return the new arrays 0 element*/
+
