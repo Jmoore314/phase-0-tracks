10d2d25ccaa50b5f424b9cdf62322bc649798bbd
John Moore <johnmoore314@gmail.com>
2016-09-09 21:10:33 -0500

    Add routes for adress, great_job and addition in app.rb

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -23,6 +23,29 @@ get '/:person_1/loves/:person_2' do
   "#{params[:person_1]} loves #{params[:person_2]}"
 end
 
+get '/contact' do
+  "Your address is #{params[:address]}"
+end
+
+get '/great_job' do
+  if name 
+    "Great job, #{params[:name]}"
+  else
+    "Great job!"
+  end
+end
+
+get '/add' do
+  a = params[:a].to_i
+  b = params[:b].to_i
+  c = a + b
+  "The result of a + b is #{c}"
+end
+
+get '/students/:name' do
+  name = db.execute("SELECT * FROM students WHERE name=?", [params[:name]])[0]
+end
+
 # write a GET route that retrieves
 # all student data
 get '/students' do
